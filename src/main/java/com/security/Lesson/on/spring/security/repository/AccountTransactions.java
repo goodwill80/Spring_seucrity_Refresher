82a59a6254940b1d9f68f10086bfda34fac673a8
Ho Kit Jonathan <kitjonathan.ho@stengg.com>
2024-03-18 16:59:00 +0800

    fixed bug

--- src/main/java/com/security/Lesson/on/spring/security/repository/AccountTransactions.java
+++ src/main/java/com/security/Lesson/on/spring/security/repository/AccountTransactions.java
@@ -3,6 +3,10 @@ package com.security.Lesson.on.spring.security.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AccountTransactions extends CrudRepository<AccountTransactions, Long> {
+
+    List<AccountTransactions> findByCustomerIdOrderByTransactionDtDesc(int customerId);
 }
